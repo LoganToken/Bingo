10c13339e42994a85d32cfd172874294c346a081
Logan Token <imsacredemail@gmail.com>
2025-03-18 21:41:46 -0400

    added new goals
    king's new goals as of 3/18/2025

--- tables/dk_rando.js
+++ tables/dk_rando.js
@@ -2,47 +2,50 @@ var bingoList = [];
 
 bingoList[1] = [
 { "id": 1, "name": "Coconut Gun", "types": [], "subtypes": ["DK Move"], "weight": 1},
-{ "id": 6, "name": "Peanut Popguns", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
-{ "id": 7, "name": "Guitar Gazump", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
-{ "id": 9, "name": "Rocketbarrel Boost", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
-{ "id": 11, "name": "Grape Shooter", "types": [], "subtypes": ["Lanky Move"], "weight": 1},
-{ "id": 16, "name": "Feather Bow", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
-{ "id": 19, "name": "Mini Monkey", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
+{ "id": 145, "name": "Find Candy", "types": [], "subtypes": ["Shopkeepers"], "weight": 1},
+{ "id": 144, "name": "Find Funky", "types": [], "subtypes": ["Shopkeepers"], "weight": 1},
+{ "id": 143, "name": "Find Cranky", "types": [], "subtypes": ["Shopkeepers"], "weight": 1},
+{ "id": 32, "name": "Super Simian Slam", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 23, "name": "Primate Punch", "types": [], "subtypes": ["Chunky Move"], "weight": 1},
 { "id": 21, "name": "Pineapple Launcher", "types": [], "subtypes": ["Chunky Move"], "weight": 1},
 { "id": 22, "name": "Triangle Trample", "types": [], "subtypes": ["Chunky Move"], "weight": 1},
-{ "id": 23, "name": "Primate Punch", "types": [], "subtypes": ["Chunky Move"], "weight": 1},
-{ "id": 32, "name": "Super Simian Slam", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 16, "name": "Feather Bow", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
+{ "id": 11, "name": "Grape Shooter", "types": [], "subtypes": ["Lanky Move"], "weight": 1},
+{ "id": 9, "name": "Rocketbarrel Boost", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
+{ "id": 7, "name": "Guitar Gazump", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
+{ "id": 6, "name": "Peanut Popguns", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
+{ "id": 19, "name": "Mini Monkey", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
 ];
 
 bingoList[2] = [
-{ "id": 81, "name": "Factory Diddy Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 78, "name": "Aztec Tiny Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 31, "name": "Barrel Throwing", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 81, "name": "Factory Diddy Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 77, "name": "Aztec Lanky Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 31, "name": "Barrel Throwing", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 17, "name": "Saxophone Slam", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
 { "id": 12, "name": "Trombone Tremor", "types": [], "subtypes": ["Lanky Move"], "weight": 1},
 { "id": 2, "name": "Bongo Blast", "types": [], "subtypes": ["DK Move"], "weight": 1},
-{ "id": 17, "name": "Saxophone Slam", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
 ];
 
 bingoList[3] = [
-{ "id": 83, "name": "Factory Tiny Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 18, "name": "Pony Tail Twirl", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
-{ "id": 20, "name": "Monkeyport", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
-{ "id": 28, "name": "Orange Throwing", "types": [], "subtypes": ["Family Move"], "weight": 1},
-{ "id": 57, "name": "30 Golden Bananas", "types": ["Golden Bananas"], "subtypes": [], "weight": 1},
-{ "id": 70, "name": "Japes DK Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 84, "name": "Factory Chunky Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 86, "name": "Galleon Diddy Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 87, "name": "Galleon Lanky Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 109, "name": "Obtain All 5 Kongs", "types": [], "subtypes": [], "weight": 1},
+{ "id": 87, "name": "Galleon Lanky Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 86, "name": "Galleon Diddy Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 84, "name": "Factory Chunky Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 83, "name": "Factory Tiny Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 57, "name": "30 Golden Bananas", "types": ["Golden Bananas"], "subtypes": [], "weight": 1},
+{ "id": 28, "name": "Orange Throwing", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 20, "name": "Monkeyport", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
+{ "id": 18, "name": "Pony Tail Twirl", "types": [], "subtypes": ["Tiny Move"], "weight": 1},
+{ "id": 70, "name": "Japes DK Medal", "types": [], "subtypes": [], "weight": 1},
 ];
 
 bingoList[4] = [
-{ "id": 89, "name": "Galleon Chunky Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 82, "name": "Factory Lanky Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 103, "name": "Castle Tiny Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 62, "name": "Have 2 Keys", "types": ["Keys"], "subtypes": [], "weight": 1},
 { "id": 100, "name": "Castle DK Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 62, "name": "Have 2 Keys", "types": ["Keys"], "subtypes": [], "weight": 1},
+{ "id": 89, "name": "Galleon Chunky Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 24, "name": "Hunky Chunky", "types": [], "subtypes": ["Chunky Move"], "weight": 1},
 { "id": 5, "name": "Baboon Blast", "types": [], "subtypes": ["DK Move"], "weight": 1},
 { "id": 25, "name": "Gorilla Gone", "types": [], "subtypes": ["Chunky Move"], "weight": 1},
@@ -50,18 +53,19 @@ bingoList[4] = [
 ];
 
 bingoList[5] = [
-{ "id": 8, "name": "Chimpy Charge", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
-{ "id": 71, "name": "Japes Diddy Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 72, "name": "Japes Lanky Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 79, "name": "Aztec Chunky Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 94, "name": "Forest Chunky Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 4, "name": "Strong Kong", "types": [], "subtypes": ["DK Move"], "weight": 1},
 { "id": 10, "name": "Simian Spring", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
-{ "id": 13, "name": "Orangstand", "types": [], "subtypes": ["Lanky Move"], "weight": 1},
-{ "id": 15, "name": "Baboon Balloon", "types": [], "subtypes": ["Lanky Move"], "weight": 1},
-{ "id": 26, "name": "Homing Ammo", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 146, "name": "Find Snide", "types": [], "subtypes": ["Shopkeepers"], "weight": 1},
 { "id": 27, "name": "Sniper Sight", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 26, "name": "Homing Ammo", "types": [], "subtypes": ["Family Move"], "weight": 1},
+{ "id": 15, "name": "Baboon Balloon", "types": [], "subtypes": ["Lanky Move"], "weight": 1},
+{ "id": 13, "name": "Orangstand", "types": [], "subtypes": ["Lanky Move"], "weight": 1},
 { "id": 3, "name": "Gorilla Grab", "types": [], "subtypes": ["DK Move"], "weight": 1},
+{ "id": 94, "name": "Forest Chunky Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 79, "name": "Aztec Chunky Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 72, "name": "Japes Lanky Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 71, "name": "Japes Diddy Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 8, "name": "Chimpy Charge", "types": [], "subtypes": ["Diddy Move"], "weight": 1},
+{ "id": 4, "name": "Strong Kong", "types": [], "subtypes": ["DK Move"], "weight": 1},
 ];
 
 bingoList[6] = [
@@ -74,20 +78,21 @@ bingoList[6] = [
 ];
 
 bingoList[7] = [
-{ "id": 92, "name": "Forest Lanky Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 91, "name": "Forest Diddy Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 88, "name": "Galleon Tiny Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 102, "name": "Castle Lanky Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 51, "name": "Mermaid's Reward", "types": [], "subtypes": [], "weight": 1},
 { "id": 49, "name": "The Bean", "types": [], "subtypes": [], "weight": 1},
+{ "id": 51, "name": "Mermaid's Reward", "types": [], "subtypes": [], "weight": 1},
 { "id": 58, "name": "40 Golden Bananas", "types": ["Golden Bananas"], "subtypes": [], "weight": 1},
+{ "id": 88, "name": "Galleon Tiny Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 91, "name": "Forest Diddy Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 92, "name": "Forest Lanky Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 102, "name": "Castle Lanky Medal", "types": [], "subtypes": [], "weight": 1},
 ];
 
 bingoList[8] = [
+{ "id": 140, "name": "5 Medals", "types": ["Medals"], "subtypes": ["CBs"], "weight": 1},
+{ "id": 73, "name": "Japes Tiny Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 33, "name": "Super Duper Simian Slam", "types": [], "subtypes": ["Multiple Moves"], "weight": 1},
 { "id": 53, "name": "Race the Owl Reward", "types": [], "subtypes": [], "weight": 1},
 { "id": 68, "name": "Complete Diddy's Lower Cabin", "types": [], "subtypes": ["Cabins"], "weight": 1},
-{ "id": 73, "name": "Japes Tiny Medal", "types": [], "subtypes": [], "weight": 1},
 ];
 
 bingoList[9] = [
@@ -96,12 +101,12 @@ bingoList[9] = [
 { "id": 121, "name": "2 Crowns", "types": ["Crowns"], "subtypes": [], "weight": 1},
 { "id": 110, "name": "Pound the X Reward", "types": [], "subtypes": [], "weight": 1},
 { "id": 90, "name": "Forest DK Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 38, "name": "Key 5", "types": [], "subtypes": ["Keys"], "weight": 1},
-{ "id": 39, "name": "Key 6", "types": [], "subtypes": ["Keys"], "weight": 1},
-{ "id": 34, "name": "Key 1", "types": [], "subtypes": ["Keys"], "weight": 1},
-{ "id": 35, "name": "Key 2", "types": [], "subtypes": ["Keys"], "weight": 1},
-{ "id": 40, "name": "Key 7", "types": [], "subtypes": ["Keys"], "weight": 1},
 { "id": 37, "name": "Key 4", "types": [], "subtypes": ["Keys"], "weight": 1},
+{ "id": 39, "name": "Key 6", "types": [], "subtypes": ["Keys"], "weight": 1},
+{ "id": 38, "name": "Key 5", "types": [], "subtypes": ["Keys"], "weight": 1},
+{ "id": 35, "name": "Key 2", "types": [], "subtypes": ["Keys"], "weight": 1},
+{ "id": 34, "name": "Key 1", "types": [], "subtypes": ["Keys"], "weight": 1},
+{ "id": 40, "name": "Key 7", "types": [], "subtypes": ["Keys"], "weight": 1},
 ];
 
 bingoList[10] = [
@@ -110,23 +115,26 @@ bingoList[10] = [
 { "id": 59, "name": "50 Golden Bananas", "types": ["Golden Bananas"], "subtypes": [], "weight": 1},
 { "id": 63, "name": "Have 3 Keys", "types": ["Keys"], "subtypes": [], "weight": 1},
 { "id": 107, "name": "Collect 1 Move for Each Kong", "types": ["Multiple Moves"], "subtypes": [], "weight": 1},
+{ "id": 44, "name": "Complete Dk Arcade Round 2", "types": [], "subtypes": [], "weight": 1},
 ];
 
 bingoList[11] = [
-{ "id": 36, "name": "Key 3", "types": [], "subtypes": ["Keys"], "weight": 1},
-{ "id": 52, "name": "Mechfish Reward", "types": [], "subtypes": [], "weight": 1},
-{ "id": 64, "name": "Defeat Mad Jack", "types": [], "subtypes": ["Keys"], "weight": 1},
+{ "id": 45, "name": "Jetpac", "types": [], "subtypes": [], "weight": 1},
+{ "id": 129, "name": "Complete Both Tiny Car Races", "types": [], "subtypes": [], "weight": 1},
 { "id": 66, "name": "Defeat Armydillo 2", "types": [], "subtypes": ["Keys"], "weight": 1},
 { "id": 123, "name": "10 Blueprints", "types": ["Blueprints"], "subtypes": [], "weight": 1},
-{ "id": 129, "name": "Complete Both Tiny Car Races", "types": [], "subtypes": [], "weight": 1},
+{ "id": 52, "name": "Mechfish Reward", "types": [], "subtypes": [], "weight": 1},
+{ "id": 36, "name": "Key 3", "types": [], "subtypes": ["Keys"], "weight": 1},
+{ "id": 64, "name": "Defeat Mad Jack", "types": [], "subtypes": ["Keys"], "weight": 1},
 ];
 
 bingoList[12] = [
-{ "id": 96, "name": "Caves Diddy Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 108, "name": "Complete All 3 Minecarts", "types": [], "subtypes": [], "weight": 1},
 { "id": 99, "name": "Caves Chunky Medal", "types": [], "subtypes": [], "weight": 1},
-{ "id": 43, "name": "1 Company Coin", "types": [], "subtypes": [], "weight": 1},
+{ "id": 147, "name": "Find All Shopkeepers", "types": ["Shopkeepers"], "subtypes": [], "weight": 1},
 { "id": 56, "name": "Race the Rabbit", "types": [], "subtypes": [], "weight": 1},
+{ "id": 43, "name": "1 Company Coin", "types": [], "subtypes": [], "weight": 1},
+{ "id": 96, "name": "Caves Diddy Medal", "types": [], "subtypes": [], "weight": 1},
 ];
 
 bingoList[13] = [
@@ -135,27 +143,28 @@ bingoList[13] = [
 { "id": 98, "name": "Caves Tiny Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 104, "name": "Castle Chunky Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 106, "name": "Collect All Moves for A Kong", "types": ["Multiple Moves"], "subtypes": [], "weight": 1},
+{ "id": 141, "name": "10 Medals", "types": ["Medals"], "subtypes": [], "weight": 1},
 ];
 
 bingoList[14] = [
 { "id": 117, "name": "Complete All Production Checks", "types": [], "subtypes": [], "weight": 1},
-{ "id": 29, "name": "Save the Apple Reward", "types": [], "subtypes": [], "weight": 1},
-{ "id": 30, "name": "Castle Lanky Tower", "types": [], "subtypes": [], "weight": 1},
-{ "id": 46, "name": "Defeat the Toy Monster", "types": [], "subtypes": ["R+D"], "weight": 1},
 { "id": 95, "name": "Caves DK Medal", "types": [], "subtypes": [], "weight": 1},
 { "id": 101, "name": "Castle Diddy Medal", "types": [], "subtypes": [], "weight": 1},
+{ "id": 30, "name": "Castle Lanky Tower", "types": [], "subtypes": [], "weight": 1},
+{ "id": 29, "name": "Save the Apple Reward", "types": [], "subtypes": [], "weight": 1},
+{ "id": 46, "name": "Defeat the Toy Monster", "types": [], "subtypes": ["R+D"], "weight": 1},
 ];
 
 bingoList[15] = [
-{ "id": 120, "name": "Complete All Tiny Temple Checks", "types": [], "subtypes": [], "weight": 1},
-{ "id": 105, "name": "Banana Fairy's Reward", "types": [], "subtypes": [], "weight": 1},
 { "id": 60, "name": "60 Golden Bananas", "types": ["Golden Bananas"], "subtypes": [], "weight": 1},
+{ "id": 105, "name": "Banana Fairy's Reward", "types": [], "subtypes": [], "weight": 1},
+{ "id": 120, "name": "Complete All Tiny Temple Checks", "types": [], "subtypes": [], "weight": 1},
 ];
 
 bingoList[16] = [
-{ "id": 61, "name": "300 Coloured Bananas in 1 Level", "types": ["CBs"], "subtypes": [], "weight": 1},
 { "id": 133, "name": "Have 4 Keys", "types": ["Keys"], "subtypes": [], "weight": 1},
 { "id": 139, "name": "All Kasplats in Crystal Caves", "types": [], "subtypes": [], "weight": 1},
+{ "id": 61, "name": "300 Coloured Bananas in 1 Level", "types": ["CBs"], "subtypes": [], "weight": 1},
 ];
 
 bingoList[17] = [
@@ -172,26 +181,27 @@ bingoList[18] = [
 ];
 
 bingoList[19] = [
-{ "id": 48, "name": "Beanstalk's Reward", "types": [], "subtypes": [], "weight": 1},
 { "id": 111, "name": "Forest DK Mushroom Cannons", "types": ["Guns"], "subtypes": [], "weight": 1},
 { "id": 127, "name": "400 Coloured Bananas in 1 Level", "types": ["CBs"], "subtypes": [], "weight": 1},
+{ "id": 48, "name": "Beanstalk's Reward", "types": [], "subtypes": [], "weight": 1},
 ];
 
 bingoList[20] = [
-{ "id": 135, "name": "70 Golden Bananas", "types": ["Golden Bananas"], "subtypes": [], "weight": 1},
 { "id": 112, "name": "Forest Lobby Battle Crown", "types": ["Guns"], "subtypes": [], "weight": 1},
+{ "id": 135, "name": "70 Golden Bananas", "types": ["Golden Bananas"], "subtypes": [], "weight": 1},
 ];
 
 bingoList[21] = [
-{ "id": 134, "name": "Have 5 Keys", "types": ["Keys"], "subtypes": [], "weight": 1},
 { "id": 115, "name": "Complete All 5 Door Igloo Checks", "types": ["Instruments"], "subtypes": [], "weight": 1},
 { "id": 124, "name": "20 Blueprints", "types": ["Blueprints"], "subtypes": [], "weight": 1},
+{ "id": 134, "name": "Have 5 Keys", "types": ["Keys"], "subtypes": [], "weight": 1},
+{ "id": 142, "name": "15 Medals", "types": ["Medals"], "subtypes": [], "weight": 1},
 ];
 
 bingoList[22] = [
+{ "id": 132, "name": "400 Coloured Bananas in Gloomy Galleon", "types": ["CBs"], "subtypes": [], "weight": 1},
 { "id": 119, "name": "Complete All R+D Checks", "types": ["R+D"], "subtypes": [], "weight": 1},
 { "id": 131, "name": "400 Coloured Bananas in Fungi Forest", "types": ["CBs"], "subtypes": [], "weight": 1},
-{ "id": 132, "name": "400 Coloured Bananas in Gloomy Galleon", "types": ["CBs"], "subtypes": [], "weight": 1},
 ];
 
 bingoList[23] = [
@@ -207,14 +217,12 @@ bingoList[24] = [
 
 bingoList[25] = [
 { "id": 67, "name": "All Keys", "types": ["Keys"], "subtypes": [], "weight": 1},
-{ "id": 42, "name": "Defeat K.Rool", "types": ["Endgame"], "subtypes": [], "weight": 1},
 { "id": 125, "name": "Disable the Blast-o-Matic", "types": ["Endgame"], "subtypes": [], "weight": 1},
+{ "id": 42, "name": "Defeat K.Rool", "types": ["Endgame"], "subtypes": [], "weight": 1},
 ];
 
 bingoList[26] = [
-{ "id": 44, "name": "Complete Dk Arcade Round 2", "types": [], "subtypes": [], "weight": 1},
 { "id": 41, "name": "Key 8", "types": [], "subtypes": ["Keys"], "weight": 1},
-{ "id": 45, "name": "Jetpac", "types": [], "subtypes": [], "weight": 1},
 ];
 
-bingoList["info"] = {version: "v2024.0"};
+bingoList["info"] = {version: "v2024.0", game: "DK64"};
